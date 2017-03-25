8ea7f15d0d5f24d02b8015d049809c797810f926
iamneal <iamneal89@gmail.com>
2017-03-25 12:16:16 -0600

    remove unneeded code, use absolute path for secret reading

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	"os"
+	"path/filepath"
 )
 
 var (
@@ -12,41 +13,38 @@ var (
 	SECRET_LOCATION_NAME = "SECRET_LOCATION"
 )
 
-func main() {
-	creds, err := GetGoogleDriveCreds()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("credentials %+v", creds)
-}
-
 type Credentials struct {
 	ClientSecret string
 	ClientId     string
 }
 
-func GetGoogleDriveCreds() (*Credentials, error) {
-	clientSecret := os.Getenv("CLIENT_SECRET")
-	clientId := os.Getenv("CLIENT_ID")
-
-	if clientSecret != "" && clientId != "" {
-		return &Credentials{
-			ClientSecret: clientSecret,
-			ClientId:     clientId,
-		}, nil
+func main() {
+	creds, err := GetGoogleDriveCreds()
+	if err != nil {
+		panic(err)
 	}
-	secretLoc := "~/.book_parser/client_secret.env"
+	fmt.Printf("credentials %+v\n", creds)
+}
+
+func GetGoogleDriveCreds() (*Credentials, error) {
+	secretLoc := "./client_secret.env"
 
 	if setLoc := os.Getenv(SECRET_LOCATION_NAME); setLoc != "" {
 		secretLoc = setLoc
 	}
-	err := godotenv.Load(secretLoc)
+	absSecretLoc, err := filepath.Abs(secretLoc)
+	fmt.Printf("secretloc: %s\n abs: %s\n", secretLoc, absSecretLoc)
 
 	if err != nil {
 		return nil, err
 	}
-	clientSecret = os.Getenv(CLIENT_SECRET_NAME)
-	clientId = os.Getenv(CLIENT_ID_NAME)
+	err = godotenv.Load(absSecretLoc)
+
+	if err != nil {
+		return nil, err
+	}
+	clientSecret := os.Getenv(CLIENT_SECRET_NAME)
+	clientId := os.Getenv(CLIENT_ID_NAME)
 
 	if clientSecret == "" || clientId == "" {
 		return nil, fmt.Errorf(`Tried to read file "%s" for env vars "%s" and "%s" but one or both were not set`)
