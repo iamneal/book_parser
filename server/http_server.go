10de95e70c9184eb2d8e120963f49ef0ee27d547
iamneal <iamneal89@gmail.com>
2017-03-26 22:40:45 -0600

    add basic gprc server that connects to rpc service

---
+++ server/http_server.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+  "flag"
+  "net/http"
+
+  "golang.org/x/net/context"
+  "github.com/grpc-ecosystem/grpc-gateway/runtime"
+  "google.golang.org/grpc"
+  gw "github.com/iamneal/book_parser/server/proto"
+)
+
+
+func Run(rpcLoc, httpLoc string) error {
+  ctx := context.Background()
+  ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
+
+  mux := runtime.NewServeMux()
+  opts := []grpc.DialOption{grpc.WithInsecure()}
+  err := gw.RegisterYourServiceHandlerFromEndpoint(ctx, mux, rpcLoc, opts)
+  if err != nil {
+    return err
+  }
+	http.HandleFunc("/", func(res http.ResponseWriter, req http.Request) {
+		res.WriteHeader(http.StatusOK)
+		res.Write([]byte("hello world")
+	})
+  return http.ListenAndServe(httpLoc, mux)
+}
+
