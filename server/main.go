b7b008be211e0bc99d501bbb5532e3f80872c859
iamneal <iamneal89@gmail.com>
2017-03-26 22:41:56 -0600

    test listing files on a grpc request

--- server/main.go
+++ server/main.go
@@ -1,3 +1,43 @@
 package server;
 
+import (
+	pb "github.com/iamneal/book_parser/server/proto"
+	drive "google.golang.org/api/drive/v3"
+	"golang.org/x/net/context"
+	"fmt"
+	"net"
+	"google.golang.org/grpc"
+)
 
+type Server struct {
+	driveServ *drive.Service
+}
+
+func NewRpcDriveService(d *drive.Service) *Server {
+	return &Server{driveServ: d}
+}
+
+func (s *Server) RunRpcServer(conn string) error {
+	listener, err := net.Listen("tcp", conn)
+	if err != nil {
+		return err
+	}
+	server := grpc.NewServer()
+
+	pb.RegisterBookParserServer(server, s)
+	return server.Serve(listener)
+}
+
+func (s *Server) PullBook(ctx context.Context, file *pb.File) (*pb.Empty, error) {
+	fmt.Printf("rpc Server recieved: %+v", file)
+
+	fileService := drive.NewFilesService(driveService)
+	list, err := fileService.List().Corpora("user").Context(context.Background()).
+		Spaces("drive").Do()
+
+	fmt.Printf("listed files: %+v\n", list)
+	for _, f := range list.Files {
+		fmt.Printf("fileId: %s\n Name: %s\n\n" ,f.Id, f.Name)
+	}
+	return &pb.Empty{}, nil
+}
