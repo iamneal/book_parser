8a9e6f7f1ce388f50d7ff30767a943f1cecdba12
iamneal <iamneal89@gmail.com>
2017-04-02 22:22:23 -0600

    watcher works

--- server/web_app/src/fetcher.js
+++ server/web_app/src/fetcher.js
@@ -1,4 +1,6 @@
-'use strict'
+
+// TODO all globals should be put in a file that both go and js can read
+let TOKEN_KEY = "GRPC-Metadata-book_parser_token"
 
 class Watcher {
   constructor() {
@@ -6,21 +8,29 @@ class Watcher {
   }
 
   getTokenFromCookie() {
+
   }
-  updateToken(tokenStr) {
+
+  updateLocalToken(tokenStr) {
     this.token = JSON.parse(tokenStr)
     console.log("the token: ", JSON.stringify(this.token, null, 2))
   }
+
+  refreshToken() {
+
+  }
+
   login() {
-    return makePostReqest("/login").then((xmlhttp) => {
-      console.log("success")
+    return this.makePostRequest("/login").then((xmlhttp) => {
+      console.log("success: " + xmlhttp.responseText)
+      this.updateToken(xmlhttp.responseText)
     }, (xmlhttp) => {
-      console.log("failure")
+      console.log("failure: " + xmlhttp.status)
     })
   }
   makePostRequest(path, paramsObj) {
     return new Promise((resolve, reject) => {
-      xmlhttp := new XmlHttpRequest()
+      let xmlhttp = new XMLHttpRequest()
       xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState === XMLHttpRequest.DONE) {
           if (xmlhttp.status === 200) {//StatusOK
@@ -30,23 +40,26 @@ class Watcher {
           }
         }
       }
-      params = null
-      if paramsObj {
+      let params = null
+      if (paramsObj) {
         // add the token to our request if it exists
         if (this.token) {
           paramsObj[TOKEN_KEY] = this.token.value
         }
         // translate params to form string
         params = Object.keys(paramsObj).
-          map((key) => key + '=' + paramsObj[key]).
-          join(&).
-          replace(/%20/g, '+')
+          map((key) => key + "=" + paramsObj[key]).
+          join("&").
+          replace(/%20/g, "+")
       }
       xmlhttp.open("POST", path)
-      xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xmlhttp.setRequestHeader(TOKEN_HEADER, this.token.value)
-      xmlhttp.send(params)
+      xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xmlhttp.setRequestHeader("Access-Control-Allow-Origin", "*")
+      //xmlhttp.setRequestHeader(TOKEN_KEY, this.token.value)
+      //xmlhttp.send(params)
+      //xmlhttp.send()
     })
   }
-    
 }
+
+module.exports.Watcher = Watcher
