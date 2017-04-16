4ef4ac89a8000c216bab5f8786b2e92995a1c1b6
iamneal <iamneal89@gmail.com>
2017-04-15 21:54:48 -0600

    get globals in a file both languages can parse

--- server/global_values.go
+++ server/global_values.go
@@ -1,7 +1,39 @@
 package server
 
-var (
-	COOKIE_NAME = "GRPC-Metadata-book_parser_token"
-	HTML_LOCATION = "./server/web_app/build/"
-	SECRET_LOCATION_NAME = "SECRET_LOCATION"
+import(
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
 )
+
+var (
+	TOKEN_KEY string
+	GRPC_GATEWAY_TOKEN string
+	HTML_LOCATION string
+	SECRET_LOCATION_NAME string
+)
+
+type Globals struct{
+	TOKEN_KEY string
+	GRPC_GATEWAY_TOKEN string
+	HTML_LOCATION string
+	SECRET_LOCATION_NAME string
+}
+
+func init() {
+	bytes, err := ioutil.ReadFile("./server/globals.json")
+	if err != nil {
+		panic(fmt.Sprintf("could not read globals file: %s", err))
+	}
+
+	var globals Globals
+	err = json.Unmarshal(bytes, &globals)
+	if err != nil {
+		panic(fmt.Sprintf("could not unmarshal json: %s", err))
+	}
+	fmt.Printf("our globals %+v\n", globals)
+	TOKEN_KEY = globals.TOKEN_KEY
+	GRPC_GATEWAY_TOKEN  = globals.GRPC_GATEWAY_TOKEN
+	HTML_LOCATION = globals.HTML_LOCATION
+	SECRET_LOCATION_NAME = globals.SECRET_LOCATION_NAME
+}
